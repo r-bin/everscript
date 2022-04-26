b88908c34f1d63f8468a5570ba6d369b60a39e24
r-bin <r-bin@github.fake>
2022-04-26 14:53:57 +0200

    prepared arithmatic

--- everscript.py
+++ everscript.py
@@ -51,8 +51,8 @@ def handle_parse(code):
         print(highlight(pformat(obj), PythonLexer(), Terminal256Formatter()))
 
     print("To parse: '"+code+"'")
-    pprint_color(list(lexer.lex(code)))
-    parsed_code = parser.parse(lexer.lex(code))
+    print(list(lexer.lex(code)))
+    parser.parse(lexer.lex(code))
     
     out = generator.generate()
     utils.dump(out, "patch.txt")
@@ -76,6 +76,15 @@ enum FLAG {
     GOURD_1 = [0x2268, 0x40]
 }
 
+enum DOG {
+    WOLF = 0x02,
+    WOLF2 = 0x04,
+    GREYHOUND = 0x06,
+    POODLE = 0x08,
+    PUPPER = 0x0a,
+    TOASTER = 0x0c
+}
+
 fun end() {
     code(0x00, "// (00) END (return)");
 }
@@ -86,10 +95,19 @@ fun fade_out() {
 fun load_map(map, x, y) {
     code(0x22, x, y, map, 0x00, "// (22) CHANGE MAP = 0x34 @ [ 0x0090 | 0x0118 ]: ...");
 }
+fun prepare_transition(direction) {
+    code(0xa3, direction, "// (a3) CALL "Prepare room change? North exit/south entrance outdoor-indoor?" (0x26)");
+}
+
 fun transition(map, x, y, direction) {
     fade_out();
-    code(0xa3, direction, "// (a3) CALL "Prepare room change? North exit/south entrance outdoor-indoor?" (0x26)");
+    prepare_transition(direction);
     load_map(x, y, map);
+}
+
+fun init_map(x_start, y_start, x_end, y_end) {
+    code(0x1b, 0x23e9 - 0x2258, 0x23eb - 0x2258, x_start, y_start);
+    code(0x1b, 0x23ed - 0x2258, 0x23ef - 0x2258, x_end, y_end);
 }
 
 @install()
@@ -112,6 +130,11 @@ fun room_1_exit_north_if() {
     };
 }
 
+@install()
+fun test() {
+    eval("11");
+    init_map(0x00, 0x02, 0x80, 0x96);
+}
 """)
 
 utils.patch('Secret of Evermore (U) [!].smc', "out/everscript.ips")
