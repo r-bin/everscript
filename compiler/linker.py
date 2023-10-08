c53e9b9b10376d5657259211bf6bfb90b722a746
r-bin <r-bin@github.fake>
2023-10-08 22:14:40 +0200

    added sky and river battle, slightly reordered rooms

--- compiler/linker.py
+++ compiler/linker.py
@@ -249,6 +249,9 @@ class MapDataHandler():
             self.MapData(0x4d, 0xad8669, 0x00, 0x00, "Antiqua - Nobilia, Inside palace (Horace cutscene)"),
             self.MapData(0x4e, 0xaab123, 0x02, 0x00, "Gothica - Ivor Tower, west alley (market)"),
             self.MapData(0x4f, 0xa99f92, 0x19, 0x10, "Antiqua - East of Crustacia"),
+            self.MapData(0x50, 0xa5fd60, 0x00, 0x00, "Prehistoria - Sky above Volcano"),
+            # …
+            self.MapData(0x53, 0xabe2f9, 0x00, 0x00, "Antiqua - Act2 Start Cutscene"),
             # …
             self.MapData(0x58, 0xad8cbb, 0x05, 0x00, "Antiqua - 'mids boss room (Rimsala)"),
             self.MapData(0x59, 0xa08000, 0x41, 0x1c, "Prehistoria - Quick sand desert"),
