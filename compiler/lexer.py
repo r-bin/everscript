b43b3f2213e358cd4d7d69f066ec845c3f5e8e95
r-bin <r-bin@github.fake>
2023-11-09 10:01:20 +0100

    added groups, added more music and enemies

--- compiler/lexer.py
+++ compiler/lexer.py
@@ -89,6 +89,7 @@ class Lexer():
         self.lexer.add('NAME_IDENTIFIER', '[a-z_][a-z0-9][a-z0-9_]*(?=\()')
         self.lexer.add('MAP', 'map(?=\ )')
         self.lexer.add('AREA', 'area(?=\ )')
+        self.lexer.add('GROUP', 'group(?=\ )')
         
         self.lexer.add('OBJECT', 'object(?=\[)')
         self.lexer.add('ARG', 'arg(?=\[)')
