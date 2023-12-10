9ab9e8992007809b1f1a8c1ef4471cf87081b39e
r-bin <r-bin@github.fake>
2023-12-10 23:37:35 +0100

    added missing maps (3 are broken)

--- compiler/linker.py
+++ compiler/linker.py
@@ -190,51 +190,55 @@ class MapDataHandler():
             self.MapData(0x06, 0x9c8000, 0x1b, 0x14, "Antiqua - Outside of 'mids"),
             self.MapData(0x07, 0xa793e9, 0x0a, 0x0d, "Antiqua - West of Crustacia"),
             self.MapData(0x08, 0xa5b42e, 0x05, 0x05, "Antiqua - Nobilia, Square"),
-            # …
+            self.MapData(0x09, 0xa9bde6, 0x03, 0x00, "Antiqua - Nobilia, Square during Aegis fight"),
             self.MapData(0x0a, 0x9faaeb, 0x0b, 0x3a, "Antiqua - Nobilia, Market"),
             self.MapData(0x0b, 0xa6964a, 0x0a, 0x0c, "Antiqua - Nobilia, Palace grounds"),
             self.MapData(0x0c, 0xad9f83, 0x04, 0x02, "Antiqua - Nobilia, Inn"),
-            # …
+            self.MapData(0x0d, 0xa9d82a, 0x08, 0x00, "Gothica - Ebon Keep Hall (Stairs, behind Verm)"),
+            self.MapData(0x0e, 0xacc8b8, 0x02, 0x01, "Gothica - Ebon Keep Dining Room"),
+            self.MapData(0x0f, 0xaac915, 0x02, 0x02, "Gothica - Ebon Keep West Room (Naris)"),
+            self.MapData(0x10, 0xadc553, 0x02, 0x00, "Gothica - Ebon Keep Stained Glass Hallway"),
+            self.MapData(0x11, 0xabbd82, 0x02, 0x00, "Gothica - Ebon Keep Queen's Room"),
             self.MapData(0x12, 0xa58000, 0x02, 0x0d, "Gothica - Ebon Keep sewers"),
             self.MapData(0x13, 0xacb18d, 0x04, 0x01, "Gothica - Between Ebon Keep sewers, Dark Forest and Swamp"),
-            # …
+            self.MapData(0x14, 0xabd077, 0x02, 0x03, "Gothica - Ebon Keep Tinker's Room"),
             self.MapData(0x15, 0xa0ff33, 0x00, 0x00, "Brian's Test Ground"),
-            # …
+            self.MapData(0x16, 0xa38000, 0x2a, 0x18, "Prehistoria - BBM"),
+            self.MapData(0x17, 0xac9955, 0x08, 0x0c, "Prehistoria - Bug room 2"),
             self.MapData(0x18, 0xab8ad2, 0x03, 0x02, "Prehistoria - Thraxx' room"),
             self.MapData(0x19, 0xa48000, 0x05, 0x13, "Gothica - Chessboard"),
-            # …
             self.MapData(0x1a, 0xa8e53c, 0x04, 0x01, "Gothica - Below chessboard"),
-            # …
+            self.MapData(0x1b, 0xa68000, 0x13, 0x00, "Antiqua - Desert of Doom"),
             self.MapData(0x1c, 0xa7e153, 0x06, 0x14, "Antiqua - Nobilia, North of Market"),
             self.MapData(0x1d, 0xa7f396, 0x00, 0x00, "Antiqua - Nobilia, Arena (Vigor Fight)"),
-            # …
+            self.MapData(0x1e, 0xacde7c, 0x01, 0x09, "Antiqua - Nobilia, Arena Holding Room"),
             self.MapData(0x1f, 0xa8d4ca, 0x02, 0x00, "Gothica - Doubles room in forest"),
-            # …
             self.MapData(0x20, 0xace592, 0x02, 0x00, "Gothica - Timberdrake room in forest"),
-            # …
+            self.MapData(0x21, 0xacf37c, 0x03, 0x00, "Gothica - Dark forest entrance (save point)"),
             self.MapData(0x22, 0xa1c650, 0x11, 0x09, "Gothica - Dark Forest"),
             self.MapData(0x23, 0xacb96b, 0x06, 0x03, "Antiqua - Halls SW"),
-            # …
+            self.MapData(0x24, 0xa3bc84, 0x1d, 0x07, "Antiqua - Halls NW"),
             self.MapData(0x25, 0xa4b92d, 0x0d, 0x14, "Prehistoria - Fire Eyes' Village"),
             self.MapData(0x26, 0xadcee5, 0x01, 0x03, "Prehistoria - West area with Defend"),
             self.MapData(0x27, 0xa6d67a, 0x03, 0x1e, "Prehistoria - Mammoth Graveyard"),
-            # …
+            self.MapData(0x28, 0xa1a38f, 0x20, 0x02, "Antiqua - Halls Collapsing Bridge"),
             self.MapData(0x29, 0xa7bb53, 0x20, 0x02, "Antiqua - Halls main room"),
             self.MapData(0x2a, 0xaa98b5, 0x01, 0x00, "Antiqua - Halls Boss Room"),
             self.MapData(0x2b, 0xa7ce91, 0x02, 0x11, "Antiqua - Outside of halls"),
             self.MapData(0x2c, 0xac911f, 0x02, 0x00, "Antiqua - Halls SE"),
-            # …
+            self.MapData(0x2d, 0xa28000, 0x12, 0x05, "Antiqua - Halls NE"),
+            self.MapData(0x2e, 0xadc8ba, 0x01, 0x02, "Antiqua - Blimp's Cave"),
             self.MapData(0x2f, 0xa0cd23, 0x06, 0x14, "Antiqua - Horace's camp"),
-            # …
             self.MapData(0x30, 0xaaa4f5, 0x08, 0x07, "Antiqua - Crustacia inside pirate ship"),
-            # …
+            self.MapData(0x31, 0xacd757, 0x00, 0x00, "Intro - Podunk 1965"),
+            self.MapData(0x32, 0xada585, 0x00, 0x00, "Intro - Podunk 1995"),
             self.MapData(0x33, 0xadb50c, 0x02, 0x01, "Strong Heart's Exterior"),
             self.MapData(0x34, 0xadbd79, 0x01, 0x03, "Prehistoria - Strong Heart's Hut"),
             self.MapData(0x35, 0xaad4ab, 0x05, 0x0a, "Act1 Quicksand, Bugmuck and Volcano caves + Act2 West Alchemy Cave"),
             self.MapData(0x36, 0xa3f774, 0x02, 0x04, "Prehistoria - Both fire pits (one room)"),
             self.MapData(0x37, 0x9dbcf3, 0x0c, 0x16, "Gothica - Gomi's Tower"),
             self.MapData(0x38, 0x9e8000, 0x02, 0x1f, "Prehistoria - South jungle / Start"),
-            # …
+            self.MapData(0x39, 0xaca984, 0x02, 0x00, "Gothica - Ebon Keep Fire pit"),
             self.MapData(0x3a, 0xadab68, 0x02, 0x00, "Antiqua - Nobilia, Fire pit"),
             self.MapData(0x3b, 0xa2c0a8, 0x04, 0x22, "Prehistoria - Volcano Room 2"),
             self.MapData(0x3c, 0xa2a161, 0x15, 0x19, "Prehistoria - Volcano Room 1"),
@@ -243,14 +247,17 @@ class MapDataHandler():
             self.MapData(0x3f, 0xaabd2c, 0x01, 0x00, "Prehistoria - Volcano Boss Room"),
             self.MapData(0x40, 0xab8000, 0x02, 0x00, "Gothica - Swamp south of Gomi's Tower"),
             self.MapData(0x41, 0xa5e6be, 0x04, 0x17, "Prehistoria - North jungle"),
-            # …
+            self.MapData(0x42, 0xa9f1d1, 0x1c, 0x02, "Omnitopia - Reactor room and Reactor control"),
+            self.MapData(0x43, 0xaba9f5, 0x01, 0x06, "Omnitopia - Control room"),
+            self.MapData(0x44, 0xaaf592, 0x02, 0x01, "Omnitopia - Greenhouse (dark or both?)"),
             self.MapData(0x45, 0xa3da2c, 0x02, 0x00, "Omnitopia - Secret boss room"),
-            # …
+            self.MapData(0x46, 0xa6eb36, 0x05, 0x03, "Omnitopia - Professor's lab and ship area, (also?) Intro"),
+            self.MapData(0x47, 0xaa8c6d, 0x01, 0x07, "Omnitopia - Storage room"),
             self.MapData(0x48, 0x9fd4e3, 0x38, 0x35, "Omnitopia - Metroplex tunnels (rimsalas, spheres)"),
             self.MapData(0x49, 0xa4ee2d, 0x0d, 0x00, "Omnitopia - Junkyard (Landing spot)"),
-            # …
             self.MapData(0x4a, 0xabebf5, 0x02, 0x08, "Omnitopia - Final Boss Room"),
-            # …
+            self.MapData(0x4b, 0xa0a80a, 0x32, 0x20, "Antiqua - Oglin cave"),
+            self.MapData(0x4c, 0xab958d, 0x02, 0x0a, "Antiqua - Nobilia, Fountain and snake statues"),
             self.MapData(0x4d, 0xad8669, 0x00, 0x00, "Antiqua - Nobilia, Inside palace (Horace cutscene)"),
             self.MapData(0x4e, 0xaab123, 0x02, 0x00, "Gothica - Ivor Tower, west alley (market)"),
             self.MapData(0x4f, 0xa99f92, 0x19, 0x10, "Antiqua - East of Crustacia"),
@@ -258,44 +265,49 @@ class MapDataHandler():
             self.MapData(0x51, 0xa9aed7, 0x09, 0x19, "Prehistoria - Village Huts and Blimp's Hut"),
             self.MapData(0x52, 0xacfa2d, 0x01, 0x00, "Prehistoria - Top of Volcano"),
             self.MapData(0x53, 0xabe2f9, 0x00, 0x00, "Antiqua - Act2 Start Cutscene"),
-            # …
+            self.MapData(0x54, 0xaa8000, 0x07, 0x03, "Omnitopia - Shops"),
+            self.MapData(0x55, 0x9ed770, 0x2f, 0x18, "Antiqua - 'mids bottom level (Dog start)"),
+            self.MapData(0x56, 0xa6c154, 0x06, 0x18, "Antiqua - 'mids top level (Boy start)"),
             self.MapData(0x57, 0xa7a7a2, 0x15, 0x0b, "Antiqua - 'mids basement level (Tiny)"),
             self.MapData(0x58, 0xad8cbb, 0x05, 0x00, "Antiqua - 'mids boss room (Rimsala)"),
             self.MapData(0x59, 0xa08000, 0x41, 0x1c, "Prehistoria - Quick sand desert"),
-            # …
+            self.MapData(0x5a, 0xad9309, 0x01, 0x02, "Prehistoria - Acid rain guy"),
             self.MapData(0x5b, 0xa78000, 0x05, 0x0b, "Prehistoria - East jungle"),
             self.MapData(0x5c, 0xa8f590, 0x03, 0x04, "Prehistoria - Raptors"),
-            # …
+            self.MapData(0x5d, 0xabd9ce, 0x07, 0x00, "Gothica - Ebon Keep Courtyard (South of Verm)"),
             self.MapData(0x5e, 0xacec95, 0x0a, 0x00, "Gothica - Ebon Keep Front Room (Verm)"),
-            # …
+            self.MapData(0x5f, 0xadb961, 0x04, 0x00, "Gothica - Ebon Keep Verm side rooms"),
             self.MapData(0x60, 0xadcc0b, 0x01, 0x03, "Gothica - Ebon Keep Storage Room"),
-            # …
+            self.MapData(0x61, 0xa8b3a9, 0x00, 0x00, "Opening - Scrolling over Machine"),
+            self.MapData(0x62, 0xa0f130, 0x09, 0x00, "Gothica - Ivor Tower, west square (trailers)"),
+            self.MapData(0x63, 0xad8000, 0x02, 0x00, "Gothica - Ivor Tower, inside trailers"),
             self.MapData(0x64, 0xadb0aa, 0x02, 0x00, "Antiqua - Cave entrance under 'mids"),
-            # …
+            self.MapData(0x65, 0x9d8000, 0x1d, 0x22, "Prehistoria - Swamp (main area)"),
+            self.MapData(0x66, 0xa8c44e, 0x07, 0x13, "Prehistoria - West of swamp"),
             self.MapData(0x67, 0x9eabdf, 0x0d, 0x2a, "Prehistoria - Bugmuck exterior"),
             self.MapData(0x68, 0xa6abe2, 0x0d, 0x00, "Antiqua - Crustacia exterior"),
             self.MapData(0x69, 0xa18000, 0x1f, 0x21, "Prehistoria - Volcano path"),
-            # …
+            self.MapData(0x6a, 0xac88e1, 0x00, 0x00, "Antiqua - Act2 Start Cutscene - waterfall"),
             self.MapData(0x6b, 0xa99047, 0x07, 0x00, "Antiqua - Waterfall"),
             self.MapData(0x6c, 0xabc707, 0x01, 0x01, "Gothica - SE of Ivor Tower (Well)"),
-            # …
             self.MapData(0x6d, 0xabb3bc, 0x00, 0x00, "Antique - Aquagoth Room"),
-            # …
+            self.MapData(0x6e, 0xa8a2f4, 0x09, 0x00, "Gothica - Ivor Tower Hall"),
+            self.MapData(0x6f, 0xaadfdf, 0x02, 0x02, "Gothica - Ivor Tower Dining Room"),
             self.MapData(0x70, 0xa9cb12, 0x0a, 0x00, "Gothica - Ivor Tower Exterior Bridges and Balconies"),
-            # …
+            self.MapData(0x71, 0x9f8000, 0x39, 0x20, "Gothica - Ivor Tower East Room + Kitchen"),
             self.MapData(0x72, 0xa88000, 0x13, 0x12, "Gothica - Ivor Tower East Upper Floor"),
-            # …
+            self.MapData(0x73, 0x9cf0c2, 0x1c, 0x00, "Gothica - Ivor Tower Dog Maze Underground"),
             self.MapData(0x74, 0xa49d43, 0x20, 0x01, "Gothica - Ebon Keep and Ivory Tower dungeon + pipe room"),
-            # …
+            self.MapData(0x75, 0xad9947, 0x02, 0x00, "Gothica - Ivor Tower Stariwell to dungeon"),
             self.MapData(0x76, 0x9dea4e, 0x02, 0x14, "Gothica - South of Ivor Tower (Gate)"),
             self.MapData(0x77, 0xaba009, 0x03, 0x00, "Gothica - Ivor Tower Puppet Show / Mungola"),
-            # …
+            self.MapData(0x78, 0xaca178, 0x04, 0x00, "Gothica - Ivor Tower Queen's Room"),
             self.MapData(0x79, 0xa5cdd9, 0x04, 0x08, "Gothica - Ivor Tower Sewers"),
             self.MapData(0x7a, 0xadc17d, 0x02, 0x00, "Gothica - Ivor Tower Sewers Exterior (landing spot)"),
             self.MapData(0x7b, 0xa59a21, 0x08, 0x07, "Gothica - Ebon Keep and Ivor Tower Exterior Bottom Half"),
             self.MapData(0x7c, 0xa2dfcb, 0x0b, 0x00, "Gothica - Ebon Keep and Ivor Tower Exterior Top Half"),
             self.MapData(0x7d, 0xa1e7e2, 0x0c, 0x15, "Gothica - Ebon Keep and Ivor Tower Interior"),
-            # …
+            self.MapData(0x7e, 0xa89236, 0x01, 0x11, "Omnitopia - Jail"),
         ]
 
         maps = dict([(map.index, map) for map in maps])
