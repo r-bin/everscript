80d9f42eeb4893f884a035adb51976990603e4fe
r-bin <r-bin@github.fake>
2024-04-10 23:47:09 +0200

    replaced scaling logic with a placeholder lookup table

--- patches/scale_enemies.asm
+++ patches/scale_enemies.asm
@@ -3,7 +3,24 @@ header
 
 ; TODO: currently adds the content of $2700/entity+0x30 to the stats, instead of scaling them properly
 
+; IDs:
+; boy = 0A26? B678? (0)
+; …
+; FE = b9f0 (12)
+; … (96)
+; flower_orange = d5b0 (108)
+; flower_purple = d5fa (109)
+; raptor_purple = d644 (110)
+; … (31)
+; carltron = df3a (141)
+
+!ENEMY_LEVEL = $2700
+
 !MODIFICATION_MEMORY = $FE8000
+!TABLE_MEMORY = $FE9000
+
+!OFFSET_FIRST_ID = $B678
+!OFFSET_TABLE_START = $d5fa-!OFFSET_FIRST_ID
 
 !ID_BOY = #$0a26
 !ID_DOG = #$0a70
@@ -39,6 +56,7 @@ org $8f868e
   JSL money_calculation ; LDA $8e0027,x (size 4)
 
 org !MODIFICATION_MEMORY
+db "+ScaleEnemies"
 hp_calculation:
   ;PHA
   PHB
@@ -51,8 +69,7 @@ hp_calculation:
   CMP !ID_DOG : BEQ .skipBoyDog
 
   LDA $8e000f,x ; original code
-  CLC
-  ADC $2700
+  CLC : ADC !ENEMY_LEVEL
 
   BRA .doneModifying
   .skipBoyDog
@@ -74,43 +91,29 @@ attack_calculation:
   ; [DURING] A:#$____ (todo) X:#$d5fa (wimpy) Y:#$401d (wimpy id #1)
   ; [OUT]    A:#$0009 (wimpy attack) X:#$d5fa (wimpy) Y:#$4e89 (boy)
 
-  PHA
-  PHB
-  PHP
-  PHX
-  PHY
+  PHA ; push "source type"
 
-  ; TXA
   CMP !ID_BOY : BEQ .skipBoyDog
   CMP !ID_DOG : BEQ .skipBoyDog
 
-  PLY
-  PLX
-  PLP
-  PLB
-  PLA
+  PLA ; pull "source type"
 
   ; without juggling the game doesn't know which ID is the source (TAX overrides that information)
-  PHY
+  PHY ; push "target id"
 
-  TXY
-  TAX
-  LDA $8e0019,x ; original code
-  CLC
-  ;ADC $2700
-  ADC $0030,y
-  ; TAX ; original code
+  TXY ; transfer "source id"
+  CLC : ADC !ENEMY_LEVEL ; example: contains 00, 02, …, 48 [49 4a] (offset in the wimpy stats table)
+  ; SEC : SBC #!OFFSET_FIRST_ID ; redundant with "LDA !TABLE_MEMORY-!OFFSET_FIRST_ID,x"
+  TAX ; tranfer "source type"
 
-  PLY
+  LDA !TABLE_MEMORY-!OFFSET_FIRST_ID,x ; example: !TABLE_MEMORY + 00 = wimpy level 1 attack
+
+  PLY ; pull "target id"
 
   BRA .doneModifying
   .skipBoyDog
 
-  PLY
-  PLX
-  PLP
-  PLB
-  PLA
+  PLA ; pull "source type"
 
   TAX ; original code
   LDA $8e0019,x ; original code
@@ -128,11 +131,10 @@ defend_calculation:
   TXA
   CMP !ID_BOY : BEQ .skipBoyDog
   CMP !ID_DOG : BEQ .skipBoyDog
-
+  
   LDA $8e001b,x ; original code
-  CLC
-  ;ADC $2700
-  ADC $0030,y
+  CLC : ADC !ENEMY_LEVEL
+  ; ADC $0030,y
 
   BRA .doneModifying
   .skipBoyDog
@@ -160,8 +162,7 @@ magic_defend_calculation:
   CMP !ID_DOG : BEQ .skipBoyDog
 
   LDA #$40 ; original code
-  SEC
-  SBC $2700
+  SEC : SBC !ENEMY_LEVEL
   SBC $8e001d,x ; original code
 
   BRA .doneModifying
@@ -191,8 +192,7 @@ experience_calculation:
   CMP !ID_DOG : BEQ .skipBoyDog
 
   LDA $8e0023,x ; original code
-  CLC
-  ADC $2700
+  CLC : ADC !ENEMY_LEVEL
 
   BRA .doneModifying
   .skipBoyDog
@@ -220,8 +220,7 @@ money_calculation:
   CMP !ID_DOG : BEQ .skipBoyDog
 
   LDA $8e0027,x ; original code
-  CLC
-  ADC $2700
+  CLC : ADC !ENEMY_LEVEL
 
   BRA .doneModifying
   .skipBoyDog
@@ -237,3 +236,15 @@ money_calculation:
   ;PLA
 
   RTL
+
+
+  org !TABLE_MEMORY
+  ; skip !OFFSET_TABLE_START
+  !counter = 0
+  while !counter < !OFFSET_TABLE_START
+    db #$ff
+    !counter #= !counter+1
+  endwhile
+  dw #$0000, #$1111, #$2222, #$3333 ; "Wimpy Flower" (109)
+
+  db "-ScaleEnemies"
