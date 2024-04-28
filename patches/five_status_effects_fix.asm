3166e0774941a61549805b9a906fe77ec60c7e57
r-bin <r-bin@github.fake>
2024-04-28 17:33:40 +0200

    added most missing status effect fixes

--- patches/five_status_effects_fix.asm
+++ patches/five_status_effects_fix.asm
@@ -13,7 +13,7 @@ org $91ad0e
   ; INY (1 bytes)
   ; INY (1 bytes)
   ; BRA $91ACF6 (2 bytes)
-  JML fix_status_effect_1 ; size 4
+  JML fix_status_effects ; size 4
   NOP
 
 org !ROM_HOOKS
@@ -33,7 +33,7 @@ macro remove_outline(flag)
   %remove_flag($009a, <flag>)
 endmacro
 
-macro fix_flag(status_id, character_flag, outline_id)
+macro fix_flag(status_id, character_byte, character_flag, outline_id)
   LDA $0046,Y ; status effect #1
   AND #$00ff
   CMP #<status_id> : BEQ ?not_fix_status
@@ -50,7 +50,7 @@ macro fix_flag(status_id, character_flag, outline_id)
   AND #$00ff
   CMP #<status_id> : BEQ ?not_fix_status
   
-  %remove_flag($0014, <character_flag>)
+  %remove_flag(<character_byte>, <character_flag>)
   %remove_outline(<outline_id>)
 
   ?not_fix_status
@@ -98,20 +98,20 @@ endmacro
 macro fix_all_status_effects()
   %fix_stat($0000, $0A3F, $0A89, $00a0, $0001) ; atlas
   %fix_stat($0018, $0A41, $0A8B, $00a2, $0010) ; defend
+  %fix_stat($0048, $0A47, $0A91, $00a4, $0008) ; speed (hit%)
+  %fix_stat($0048, $0A45, $0A8F, $00a6, $0008) ; speed (evasion)
 
-  %fix_flag($0008, $0001, $0002) ; aura
-  %fix_flag($0010, $0004, $0004) ; barrier
-  ; %fix_flag($0020, $????, $0020) ; energize (neither flag nor stat)
-  %fix_flag($0028, $0002, $0040) ; force_field
-  %fix_flag($0030, $0040, $0080) ; reflect
-  ; %fix_flag($0038, $????, $0100) ; shield (flag byte 13)
-  ; %fix_flag($0040, $????, $0400) ; regrowth (neither flag nor stat)
-  ; %fix_flag($0048, $????, $0008) ; speed (2 stats)
-  ; %fix_flag($0050, $????, $0200) ; regenerate (neither flag nor stat)
-  ; %fix_flag($0058, $????, $0200) ; pixie dust (neither flag nor stat)
+  %fix_flag($0008, $0014, $0001, $0002) ; aura
+  %fix_flag($0010, $0014, $0004, $0004) ; barrier
+  ; %fix_flag($0020, $0014, $????, $0020) ; energize (neither flag nor stat)
+  %fix_flag($0028, $0014, $0002, $0040) ; force_field
+  %fix_flag($0030, $0014, $0040, $0080) ; reflect
+  %fix_flag($0038, $0013, $0020, $0100) ; shield
+  ; %fix_flag($0040, $0014, $????, $0400) ; regrowth (neither flag nor stat)
+  %fix_flag($0050, $00ac, $0001, $0200) ; pixie dust (neither flag nor stat)
 endmacro
 
-fix_status_effect_1:
+fix_status_effects:
   ; IN: A=??, X=0, Y=4e89=target(BOY)
 
   PHA
